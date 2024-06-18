21f92afb6b9ecdda866f0f4523f27c554d7e0199
director87 <director87@foxmail.com>
2024-06-18 17:37:48 +0800

    Update amm.py

--- tool/amm.py
+++ tool/amm.py
@@ -18,19 +18,6 @@ def ExponentialProjection(x, lamda=1.0):
     #     x_out = torch.exp(-(x - mean) ** 2 / (2 * mean ** 2 * x)) * math.sqrt(1 / (2 * math.pi * math.pow(-x, 3)))
     return x_out
 
-def PsuedoRayleighProjection(x, std):
-    x_out = (1 / std) * torch.exp(- x ** 2 / (2 * std ** 2))
-    return x_out
-
-def LaplaceProjection(x, mean, beta=1.0):
-    x_out = (1 / (2 * beta)) * torch.exp(- torch.abs(x - mean) / beta)
-    return x_out
-
-def conversetanh(x):
-    x_out = 1 / (2 * math.pi) * 1 / torch.cosh(-x / 2)**2
-    return x_out+1
-
-
 
 class BasicConv(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
@@ -83,20 +70,11 @@ class ChannelGate(nn.Module):
             else:
                 channel_att_sum = channel_att_sum + channel_att_raw
 
-        # Gauss modulation
         mean = torch.mean(channel_att_sum).detach()
         std = torch.std(channel_att_sum).detach()
         # print(channel_att_sum.shape)
         sig = 1 - F.softmax(torch.max(channel_att_sum) - channel_att_sum, dim=0)
-        # print(sig)
-        # scale = GaussProjection(channel_att_sum, mean, std).unsqueeze(2).unsqueeze(3).expand_as(x)
-        # scale = PsuedoRayleighProjection(channel_att_sum, std).unsqueeze(2).unsqueeze(3).expand_as(x)
-        # scale = conversetanh(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
         scale = ExponentialProjection(channel_att_sum, lamda=0.8).unsqueeze(2).unsqueeze(3).expand_as(x)
-        # scale = LaplaceProjection(channel_att_sum, mean, beta=1.0).unsqueeze(2).unsqueeze(3).expand_as(x)
-        # print(scale)
-
-        # scale = scale / torch.max(scale)
         return x * scale
         # return x * channel_att_sum
 
@@ -118,16 +96,10 @@ class SpatialGate(nn.Module):
         x_pool = self.pool(x)
         x_out = self.spatial(x_pool)
 
-        # Gauss modulation
         mean = torch.mean(x_out).detach()
         std = torch.std(x_out).detach()
         sig = 1 - F.softmax(torch.max(x_out) - x_out, dim=0)
-        # scale = GaussProjection(x_out, mean, std)
         scale = ExponentialProjection(x_out, lamda=0.8)
-        # scale = LaplaceProjection(x_out, mean, beta=1.0)
-
-
-        # scale = scale / torch.max(scale)
         return x * scale
         # return x * x_out
 
@@ -140,32 +112,5 @@ class AMM(nn.Module):
 
     def forward(self, x):
         x_out = self.ChannelAMM(x)
-        # print(x_out.shape)
         x_out = self.SpatialAMM(x_out)
-        # print(x_out.shape)
-
-        # x_out = self.SpatialAMM(x)
-        return x_out
-
-class SCA(nn.Module):
-    def __init__(self, gate_channels, reduction_ratio=16):
-        super(SCA, self).__init__()
-        self.ChannelAMM_avg = ChannelGate(gate_channels, reduction_ratio, pool_types=['avg'])
-        self.ChannelAMM_max = ChannelGate(gate_channels, reduction_ratio, pool_types=['max'])
-        self.SpatialAMM = SpatialGate()
-
-    def forward(self, x):
-        x_avg = self.ChannelAMM_avg(x)
-        x_max = self.ChannelAMM_max(x)
-        x_c = (x_avg + x_max)/2
-        # x_c = (x_avg + x_max)
-        x_s = self.SpatialAMM(x)
-        # x_sc = self.SpatialAMM((self.ChannelAMM_avg(x) + self.ChannelAMM_max(x))/2)
-        # x_sc = self.SpatialAMM((self.ChannelAMM_avg(x)))
-        x_sc = self.SpatialAMM((self.ChannelAMM_avg(x) + self.ChannelAMM_max(x)))
-        x_out = (x_sc + x_s + x_c) / 3
-        # x_out = x_s + x_c
-        # print(x_out.shape)
-        # x_out = self.SpatialAMM(x_out)
-        # x_out = self.SpatialAMM(x)
         return x_out
